c7ce94ae9687c115b5a6313660d088ad32facc12
hihoak <artem.mihaylov00@gmail.com>
2022-08-22 13:14:32 +0300

    resolve homework 8

---
+++ hw08_envdir_tool/envreader/env_reader.go
@@ -0,0 +1,69 @@
+package envreader
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+type Environment map[string]EnvValue
+
+// EnvValue helps to distinguish between empty files and files with the first empty line.
+type EnvValue struct {
+	Value      string
+	NeedRemove bool
+}
+
+var stringIsNotEmptyRegex = regexp.MustCompile(`\S+`)
+
+// ReadDir reads a specified directory and returns map of env variables.
+// Variables represented as files where filename is name of variable, file first line is a value.
+func ReadDir(dir string) (Environment, error) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("can't read directory '%s'", dir))
+	}
+	if err = os.Chdir(dir); err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("can't change directory to '%s'", dir))
+	}
+
+	additionalEnvs := make(Environment)
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		envFile, err := os.Open(file.Name())
+		if err != nil {
+			return nil, errors.Wrap(err, fmt.Sprintf("can't open file '%s'", file.Name()))
+		}
+
+		buffer := bufio.NewReader(envFile)
+		envValue, err := buffer.ReadString('\n')
+		if err != nil && !errors.Is(err, io.EOF) {
+			return nil, errors.Wrap(err, fmt.Sprintf("can't read file '%s'", file.Name()))
+		}
+		if stringIsNotEmptyRegex.MatchString(envValue) {
+			envValue = strings.TrimSuffix(envValue, "\n")
+			// normalizing string to pass tests. replacing NULL-byte to new line byte
+			envValue = string(bytes.ReplaceAll([]byte(envValue), []byte("\x00"), []byte("\n")))
+		} else {
+			envValue = ""
+		}
+
+		_, exists := os.LookupEnv(envFile.Name())
+
+		additionalEnvs[envFile.Name()] = EnvValue{
+			Value:      envValue,
+			NeedRemove: exists,
+		}
+	}
+
+	return additionalEnvs, nil
+}
